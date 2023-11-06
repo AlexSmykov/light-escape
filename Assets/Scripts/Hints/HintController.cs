436ea2b1fc5d3bec19a0652ca3d6488945f59dbf
twoics <avozhzhov-ki20@stud.sfu-kras.ru>
2023-11-06 21:43:43 +0700

    feat: Добавил код посказок

---
+++ Assets/Scripts/Hints/HintController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+using Random = System.Random;
+
+namespace Hints
+{
+    public class HintController : MonoBehaviour
+    {
+        public int ID;
+        public float X;
+        public float Y;
+        public bool IsAdministrative;
+        public string Message;
+        public string AuthorToken;
+        public DateTime Created;
+        public bool LikedByMe;
+        public bool IsMine;
+
+
+        public void InitFromMapping(HintGet hint)
+        {
+            ID = hint.id;
+            X = hint.x;
+            Y = hint.y;
+            IsAdministrative = hint.is_administrative;
+            Message = hint.message;
+            AuthorToken = hint.author_token;
+            Created = hint.created;
+            LikedByMe = hint.liked_by_me;
+            IsMine = hint.is_mine;
+        }
+        
+        public void InitFromMapping(HintCreate hint)
+        {
+            ID = new Random().Next();
+            X = hint.x;
+            Y = hint.y;
+            IsAdministrative = false;
+            Message = hint.message;
+            AuthorToken = hint.author_token;
+            Created = DateTime.Now;
+            LikedByMe = true;
+            IsMine = true;
+        }
+
+        public string GetHintMessage()
+        {
+            return Message;
+        }
+    }
+}
---
+++ Assets/Scripts/Hints/HintsFiller.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Hints
+{
+    public class HintsFiller: MonoBehaviour
+    {
+        public GameObject obj;
+        private const string GetHintURL = "https://twoics.fvds.ru/api/hint/?user_token=";
+
+        public IEnumerator FillHints(string userToken)
+        {
+            using (UnityWebRequest webRequest = UnityWebRequest.Get(GetHintURL + userToken))
+            {
+                yield return webRequest.SendWebRequest();
+                switch (webRequest.result)
+                {
+                    case UnityWebRequest.Result.ConnectionError:
+                    case UnityWebRequest.Result.DataProcessingError:
+                        Debug.LogError("Something went wrong while make call");
+                        break;
+                    case UnityWebRequest.Result.Success:
+                        var hints = JsonConvert.DeserializeObject<List<HintGet>>(webRequest.downloadHandler.text);
+                        foreach (var hint in hints)
+                        {
+                            var created = Instantiate(obj, new Vector3(hint.x, hint.y, 0), Quaternion.identity);
+                            created.GetComponent<HintController>().InitFromMapping(hint);
+                        }
+
+                        break;
+                }
+            }
+        }
+    }
+}
