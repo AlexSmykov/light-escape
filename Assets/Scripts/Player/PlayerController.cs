77eddb6bd7288f8088ac22e25f47793ebd6805ed
Alexey Kovalyuk <nehochubanan@gmail.com>
2023-11-06 22:16:18 +0700

    Add hpbar

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 [RequireComponent(typeof(Collider2D))]
@@ -19,6 +20,7 @@ public class PlayerController : MonoBehaviour
     public float pickaxeDamage = 1;
     public float loot = 1;
     public float darknessSpeed = 1;
+    public Image hpbar;
 
     private PlayerResourcesController playerResourcesController;
 
@@ -47,6 +49,7 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         playerHpCurrent = Mathf.Min(playerHpStart, playerHpCurrent += 0.001f);
+        hpbar.fillAmount = playerHpCurrent / playerHpStart;
     }
 
     public void PlayerDamaged(float damage)
